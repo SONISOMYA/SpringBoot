7640a8a7b0dd8a9d2ed6523a0c54ae0389bf7b63
Somya Soni <somyasoni0411@gmail.com>
2025-06-22 13:22:01 +0530

    letsGO

---
+++ src/main/java/com/example/HireOrbit/HireOrbitApplication.java
@@ -0,0 +1,13 @@
+package com.example.HireOrbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireOrbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireOrbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HireOrbit/HireOrbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HireOrbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireOrbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
